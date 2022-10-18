1e952ced8c392ca68053a3d64bdf6f744c4a2d02
Ailakks <hola@cortmnzz.com>
2022-10-18 18:56:42 +0200

    feat: Added honey menu interaction management

--- src/main/java/com/cortmnzz/honeymenu/listeners/InventoryClickListener.java
+++ src/main/java/com/cortmnzz/honeymenu/listeners/InventoryClickListener.java
@@ -7,13 +7,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+import java.util.Objects;
+
 public class InventoryClickListener implements Listener {
     @EventHandler
     public void onInventoryClick(final InventoryClickEvent event) {
         if (event.getWhoClicked() instanceof Player) {
             final HoneyPlayer honeyPlayer = HoneyMenuManager.getHoneyMenuManager().getHoneyPlayerManager().getPlayer(event.getWhoClicked().getUniqueId());
-
-            
         }
     }
 }
--- src/main/java/com/cortmnzz/honeymenu/menu/HoneyMenu.java
+++ src/main/java/com/cortmnzz/honeymenu/menu/HoneyMenu.java
@@ -1,14 +1,17 @@
 package com.cortmnzz.honeymenu.menu;
 
+import com.cortmnzz.honeymenu.managers.HoneyMenuInteractionManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 public class HoneyMenu {
+    private final HoneyMenuInteractionManager honeyMenuInteractionManager;
     private final Inventory bukkitInventory;
 
     public HoneyMenu(final String title, int size) {
         this.bukkitInventory = Bukkit.createInventory(null, size, title);
+        this.honeyMenuInteractionManager = new HoneyMenuInteractionManager();
     }
     public void open(final Player bukkitPlayer) {
         bukkitPlayer.openInventory(bukkitInventory);
