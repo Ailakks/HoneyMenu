e2cfca5c32e18c96d401381d9ddf2db2a8e9bb82
Ailakks <hola@cortmnzz.com>
2022-10-18 18:59:35 +0200

    feat: Added event registration

--- src/main/java/com/cortmnzz/honeymenu/HoneyMenuManager.java
+++ src/main/java/com/cortmnzz/honeymenu/HoneyMenuManager.java
@@ -1,25 +1,23 @@
 package com.cortmnzz.honeymenu;
 
-import com.cortmnzz.honeymenu.managers.HoneyMenuInteractionManager;
+import com.cortmnzz.honeymenu.listeners.InventoryClickListener;
 import com.cortmnzz.honeymenu.managers.HoneyPlayerManager;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 public class HoneyMenuManager {
     @Getter
     private static HoneyMenuManager honeyMenuManager;
 
-    private final Plugin plugin;
-
     @Getter
     private final HoneyPlayerManager honeyPlayerManager;
-    private final HoneyMenuInteractionManager honeyMenuInteractionManager;
 
     public HoneyMenuManager(final Plugin plugin) {
         this.honeyMenuManager = this;
-        this.plugin = plugin;
 
         this.honeyPlayerManager = new HoneyPlayerManager();
-        this.honeyMenuInteractionManager = new HoneyMenuInteractionManager();
+
+        Bukkit.getPluginManager().registerEvents(new InventoryClickListener(), plugin);
     }
 }
