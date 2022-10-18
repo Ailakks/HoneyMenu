35b970599cf1ee1b7a086697c2888f7aed53ca87
Ailakks <hola@cortmnzz.com>
2022-10-18 18:50:37 +0200

    feat: Added player last opened menu

--- src/main/java/com/cortmnzz/honeymenu/player/HoneyPlayer.java
+++ src/main/java/com/cortmnzz/honeymenu/player/HoneyPlayer.java
@@ -1,9 +1,16 @@
 package com.cortmnzz.honeymenu.player;
 
+import com.cortmnzz.honeymenu.menu.HoneyMenu;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
 public class HoneyPlayer {
     private final UUID uuid;
+
+    @Getter @Setter
+    private HoneyMenu currentOpenedMenu;
 
     public HoneyPlayer(final UUID uuid) {
         this.uuid = uuid;
